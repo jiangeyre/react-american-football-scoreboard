64dcbca103f2d872020bdecf0a53a2d30b6d1b6c
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-11-04 13:07:11 -0800

    Added a countdown from 60 seconds, added an edit the team names and quarter countter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import Timer from "./Timer";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  
+  const addHomeTouchdown = e => {
+    setHomeScore(homeScore + 7);
+  }
+  const addHomeFieldGoal = e => {
+    setHomeScore(homeScore + 3);
+  }
+  const addAwayTouchdown = e => {
+    setAwayScore(awayScore + 7);
+  }
+  const addAwayFieldGoal = e => {
+    setAwayScore(awayScore + 3);
+  }
+
+  const [homeName, setHomeName] = useState('Home');
+  const [awayName, setAwayName] = useState('Away');
 
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 contentEditable='true' className="home__name">{homeName}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <Timer />
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 contentEditable='true' className="away__name">{awayName}</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
@@ -30,12 +47,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={addHomeTouchdown}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={addHomeFieldGoal}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setHomeScore(awayScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={addAwayTouchdown}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={addAwayFieldGoal}>Away Field Goal</button>
         </div>
       </section>
     </div>
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [gameQuarter, setGameQuarter] = useState(0);
+  const changeQuarter = e => {
+    setGameQuarter(gameQuarter + 1);
+    if (gameQuarter === 4){
+      setGameQuarter(1);
+    }
+  }
+
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,7 +26,7 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value" onClick={changeQuarter}>{gameQuarter}</div>
       </div>
     </div>
   );
